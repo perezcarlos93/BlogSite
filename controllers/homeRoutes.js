45fb9f4363ed8ece32d06b5c50275ecb59a2550c
perezcarlos93 <perez.carlos_a@yahoo.com>
2020-12-28 19:18:58 -0800

    worked on front end and created routes to post blogs on homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,20 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    res.render('homepage');
+    const publicPosts = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const posts = publicPosts.map((posts) => posts.get({plain: true}));
+
+    res.render('homepage', {
+      posts,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
